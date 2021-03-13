aa4b98230b3b5aa81aef6b48e68b75c405d23412
Quetier Thomas <mamangvomi@gmail.com>
2021-03-13 14:43:50 +0100

    [ADD] Inscription V1 : nav entre connexion et inscription

---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/Pages/InscriptionPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PizzaIllico.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PizzaIllico.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InscriptionPage : ContentPage
+    {
+        public InscriptionPage()
+        {
+            InitializeComponent();
+            BindingContext = new InscriptionViewModel();
+
+        }
+    }
+}
